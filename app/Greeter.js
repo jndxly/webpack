24e8b38cd525e327e138d14116df6159dcae9cae
jndxly <jndxly@126.com>
2017-12-14 21:16:43 +0800

    添加css-loader，手动import进来

--- app/Greeter.js
+++ app/Greeter.js
@@ -1,11 +1,12 @@
 // Greeter.js
 var config = require('./config.json');
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import styles from './Greeter.css';//导入
 
 class Greeter extends Component{
   render() {
     return (
-      <div>
+      <div className={styles.root}>
         {config.greetText}
       </div>
     );
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  devtool: 'cheap-source-map',
+  devtool: 'eval-source-map',
     entry:  __dirname + "/app/main.js",//已多次提及的唯一入口文件
     output: {
       path: __dirname + "/public",//打包后的文件存放的地方
@@ -11,16 +11,34 @@ module.exports = {
       inline: true//实时刷新
     },
     module:{
-      rules:[{
-        test:/(\.js|\.jsx)$/,
-        use:{
-          loader:"babel-loader",
-          options:{
-              presets:["env","react"]
-          }
+      rules: [
+        {
+            test: /(\.jsx|\.js)$/,
+            use: {
+                loader: "babel-loader"
+            },
+            exclude: /node_modules/
         },
-        exclude: /node_modules/
-      }]
+        {
+            test: /\.css$/,
+            use: [
+                {
+                    loader: "style-loader"
+                }, {
+                  loader: "css-loader",
+                  options: {
+                      modules: true, // 指定启用css modules
+                      localIdentName: '[name]__[local]--[hash:base64:5]' // 指定css的类名格式
+                  }
+
+                    
+                }
+            ]
+        }
+
+
+    ]
+
     }
   }
   
