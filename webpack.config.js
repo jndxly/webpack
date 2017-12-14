30f95142601ca2dac71bc7bdbcaa9f25559f5c57
jndxly <jndxly@126.com>
2017-12-14 21:29:37 +0800

    添加postcss，处理浏览器兼容性

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 module.exports = {
   devtool: 'eval-source-map',
     entry:  __dirname + "/app/main.js",//已多次提及的唯一入口文件
@@ -32,14 +33,19 @@ module.exports = {
                   }
 
                     
-                }
+                }, {
+                  loader: "postcss-loader"
+              }
             ]
         }
 
 
     ]
 
-    }
+    },
+    plugins: [
+        new webpack.BannerPlugin('版权所有，翻版必究')
+    ]
   }
   
   
